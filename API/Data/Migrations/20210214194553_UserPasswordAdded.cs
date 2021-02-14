a5c0636934615ae8ffa85f4126a69f468106d518
Frankcs96 <fcsuarez96@gmail.com>
2021-02-14 21:32:09 +0100

    Added Register endpoint, DTO for register and BaseApiController.cs

---
+++ API/Data/Migrations/20210214194553_UserPasswordAdded.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PassWordHash",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PassWordSalt",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PassWordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PassWordSalt",
+                table: "Users");
+        }
+    }
+}
